92f259f0248a882ea99753c845efd67ea87b8d61
Athavale, Aditya <asathavale@paypal.com>
2019-09-10 11:45:10 -0400

    javascript Prework

---
+++ Javascript Basics/javascript.js
@@ -0,0 +1,17 @@
+function growBox() {
+        document.getElementById("box").style.height = "250px";
+}
+
+function blueBox() {
+    document.getElementById("box").style.backgroundColor = "blue";
+}
+
+function fadeBox() {
+    document.getElementById("box").style.opacity = "0.6";
+}
+
+function resetBox() {
+    document.getElementById("box").style.backgroundColor = "darkorange";
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.opacity = "1.0";
+}
